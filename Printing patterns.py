7c4a690f68735ae22811e6fa7d12803732243308
ananthphyton <45979916+ananthphyton@users.noreply.github.com>
2018-12-31 07:49:47 -0800

    Add files via upload

---
+++ Printing patterns.py
@@ -0,0 +1,14 @@
+#Print traingle with #
+
+for i in range(4):
+    for j in range(i+1):
+        print("#",end="")
+    print()
+
+#exercise two is printing a,ab,abc,abcd in traingle format.
+#Need to analyze more to complete the exercise.
+x=('a','b','c','d')
+
+for i in x:
+    print(i,end="")
+
